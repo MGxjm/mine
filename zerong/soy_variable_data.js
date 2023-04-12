3f739aa5a40889afc5a31aad7e97ef3eb5d3d313
MGxjm <71872590+MGxjm@users.noreply.github.com>
2023-04-12 17:05:32 +0800

    Delete soy_variable_data.js

--- zerong/soy_variable_data.js
+++
@@ -1,8 +0,0 @@
-module.exports = {"code":200, 
-"variable_data":{ 
-"wkllq":[{ //悟空浏览器
-    "device_id":"409***", //链接上找device_id值
-    "x-tt-token":"00b***-1.0.1", //请求头的值
-    "sessionid":"b9c***"} //请求头cookie上的值
-]
-}}
